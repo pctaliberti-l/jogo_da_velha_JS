836ec8e4e47ae6375a7ecae3f92960248e91d32f
pctaliberti-l <pctaliberti-l@pop-os.localdomain>
2020-07-20 18:58:56 -0300

    atualizando

--- js/script.js
+++ js/script.js
@@ -1,32 +1,20 @@
 // definindo elementos essenciais para os jogos
-let x = document.querySelector('.x');
-let o = document.querySelector('.O');
-let boxes = document.querySelectorAll('.box'); // grade do jogo
-let buttons = document.querySelectorAll('#buttons-container button');
-let messageContainer = document.querySelector('#message');
-let messageText = document.querySelector('#message p');
+let x = document.querySelector(".x");
+let o = document.querySelector(".O");
+let boxes = document.querySelectorAll(".box"); // grade do jogo
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
 let secondPlayer; // dependendo modo de jogo
 
 // contator de jogadas (X e O)
 let player1 = 0;
 let player2 = 0;
 
-// verifica quem irá jogar
-function checkEl(player1, player2) {
-  if (player1 == player2) {
-    // X
-    el = x;
-  } else {
-    // O
-    el = o;
-  }
-  return el;
-}
-
 // adicionando click event aos boxes
 for (let i = 0; i < boxes.length; i++) {
   // clicando na caixa
-  boxes[i].addEventListener('click', function () {
+  boxes[i].addEventListener("click", function () {
     // inicio do jogo
     let el = checkEl(player1, player2); // definindo o X ou O
 
@@ -42,4 +30,16 @@ for (let i = 0; i < boxes.length; i++) {
       }
     }
   });
+}
+
+// verifica quem irá jogar
+function checkEl(player1, player2) {
+  if (player1 == player2) {
+    // X
+    el = x;
+  } else {
+    // O
+    el = o;
+  }
+  return el;
 }
